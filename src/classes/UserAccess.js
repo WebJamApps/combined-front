1fc89fa69f08206725e470511ae49ebaab65d9c6
Josh Sherman <joshua.v.sherman@gmail.com>
2017-04-25 08:52:54 -0400

    reader is inside the dashboard now

--- src/classes/UserAccess.js
+++ src/classes/UserAccess.js
@@ -22,10 +22,17 @@ export class UserAccess {
       const tempRoles = this.appState.getRoles(); //TODO: Be sure that the roles have been set.
       console.log(tempRoles);
       console.log(routingContext.fragment);
+      
       if (routingContext.fragment === '/dashboard'){
         console.log('I am only trying to go to the main dashboard');
         return next();
       }
+      
+      console.log('the child route is: ' + routingContext.params.childRoute);
+      if (routingContext.params.childRoute === 'reader'){
+        return next();
+      }
+      
       for (let i = 0; i < tempRoles.length; i++) {
         // in this case the user is only in one role at a time.
         if (routingContext.params.childRoute === tempRoles[i].toLowerCase()){
--- src/dashboard-router.js
+++ src/dashboard-router.js
@@ -7,6 +7,7 @@ export class DashboardRouter {
       { route: '', name: 'dashboard', moduleId: PLATFORM.moduleName('./dashboard'), nav: false, title: 'Dashboard', auth: true},
       { route: 'ohaf', name: 'ohaf', moduleId: PLATFORM.moduleName('./dashboard-child-routes/ohaf-router'), nav: true, title: 'OHAF', auth: true},
       { route: 'sc2rs', name: 'sc2rs', moduleId: PLATFORM.moduleName('./dashboard-child-routes/sc2rs-router'), nav: true, title: 'SC2RS', auth: true},
+      { route: 'reader', name: 'reader', moduleId: PLATFORM.moduleName('./dashboard-child-routes/reader'), nav: true, title: 'Reader', auth: true},
       { route: 'developer', name: 'developer', moduleId: PLATFORM.moduleName('./dashboard-child-routes/developer-router'), true: false, title: 'Developer', auth: true}
     ]);
     this.router = router;
