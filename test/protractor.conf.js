d7ecf678e3ebe8c0be02cd263f5e4b3aaea3be2f
Josh Sherman <joshua.v.sherman@gmail.com>
2017-04-27 14:39:47 -0400

    protractor is running

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -2,21 +2,21 @@ const port = 19876;
 
 exports.config = {
   port: port,
-
+  
   baseUrl: `http://localhost:${port}/`,
-
+  
   // use `npm start -- e2e`
-
+  
   specs: [
     '**/*.e2e.js'
   ],
-
+  
   exclude: [],
-
+  
   framework: 'jasmine',
-
+  
   allScriptsTimeout: 110000,
-
+  
   jasmineNodeOpts: {
     showTiming: true,
     showColors: true,
@@ -24,21 +24,24 @@ exports.config = {
     includeStackTrace: false,
     defaultTimeoutInterval: 400000
   },
-
+  
+  SELENIUM_PROMISE_MANAGER: false,
+  
   directConnect: true,
-
+  
   capabilities: {
     'browserName': 'chrome',
     'chromeOptions': {
       'args': ['show-fps-counter=true']
     }
   },
-
+  
   onPrepare: function() {
     process.env.BABEL_TARGET = 'node';
+    process.env.IN_PROTRACTOR = 'true';
     require('babel-register');
   },
-
+  
   plugins: [{
     package: 'aurelia-protractor-plugin'
   }]
