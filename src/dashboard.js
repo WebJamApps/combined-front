6fc77b4f5586d724cc800850616a555d33329a28
Josh Sherman <joshua.v.sherman@gmail.com>
2017-05-24 16:23:56 -0400

    new user form validation is working, but dashboard unit test is failing

--- src/dashboard.js
+++ src/dashboard.js
@@ -2,24 +2,59 @@ import {inject} from 'aurelia-framework';
 import {App} from './app';
 import {AuthService} from 'aurelia-auth';
 import {HttpClient, json} from 'aurelia-fetch-client';
-@inject(AuthService, HttpClient, App)
+import { ValidationControllerFactory, ValidationRules, Validator, validateTrigger } from 'aurelia-validation';
+//import { ValidationControllerFactory, ValidationRules, Validator } from 'aurelia-validation';
+class FormValidator {
+  constructor(validator, cb) {
+    this.validator = validator;
+    this.cb = cb;
+  }
+  /**
+  * This method will be called by the controller when validating a specific field. For instance,
+  * when the user is interacting with the title input. So, we need to validate the whole form
+  * and call our callback in order for TodoPage.canSave to be correctly updated.
+  */
+  validateProperty(object, propertyName, rules) {
+    let validationDefered = this.validator.validateProperty(object, propertyName, rules);
+    validationDefered.then(() => this.validateObject(object, rules));
+
+    return validationDefered;
+  }
+
+  /**
+  * Each time the whole form is validated, we call the registered callback to do whatever
+  * the user wants to do with the results of the validation. In our case: update
+  * TodoPage.canSave.
+  */
+  validateObject(object, rules) {
+    return this.validator.validateObject(object, rules).then(results => {
+      this.cb(results);
+      return results;
+    });
+  }
+
+  /**
+  * Implemented so the interface is complete.
+  */
+  ruleExists(rules, rule) {
+    return this.validator(rules, rule);
+  }
+}
+
+@inject(AuthService, HttpClient, App, ValidationControllerFactory, Validator)
 export class Dashboard {
-  constructor(auth, httpClient, app){
+  controller = null;
+  validator = null;
+  constructor(auth, httpClient, app, controllerFactory, validator){
     this.app = app;
     this.auth = auth;
     this.httpClient = httpClient;
-    // this.validator = {
-    //   validatorName: 'my-custom-validator',
-    //   validatorType: 'validator',
-    //   validate: function(value) {console.log('I am validating');}
-    // };
-    // this.form = new Polymer.IronMeta({
-    //   type: this.validator.validatorType,
-    //   key: this.validator.validatorName,
-    //   value: this.validator
-    // });
-    //console.log('options for user roles: ' + JSON.parse(process.env.userRoles).roles);
+    this.validator = new FormValidator(validator, results => this.updateCanSubmit(results)); //if the form is valid then set to true
+    this.controller = controllerFactory.createForCurrentScope(this.validator);
+    this.controller.validateTrigger = validateTrigger.changeOrBlur;
+    this.canSubmit = false;  //the button on the form
   }
+
   selectedValue;
   userTypes=JSON.parse(process.env.userRoles).roles;
 
@@ -29,13 +64,24 @@ export class Dashboard {
     this.user = await this.app.appState.getUser(uid);
     console.log('this is the user ' + this.user.name);
     this.childRoute();
+    this.setupValidation();
   }
 
-  DropdownChanged(changedVal) {
-    //alert(changedVal);
+  updateCanSubmit(validationResults) {
+    let valid = true;
+    for (let result of validationResults) {
+      valid = valid && result.valid;
+    }
+    this.canSubmit = valid;
+    if (this.user.userType !== '' && this.canSubmit){
+      let nub = document.getElementById('newUserButton');
+      nub.style.display = 'block';
+    }
+  }
+
+  dropdownChanged() {
     let nub = document.getElementById('newUserButton');
-    console.log(nub);
-    if (this.selectedValue !== ''){
+    if (this.user.userType !== '' && this.canSubmit){
       nub.style.display = 'block';
     } else {
       nub.style.display = 'none';
@@ -69,10 +115,21 @@ export class Dashboard {
     }
   }
 
+  setupValidation() {
+    ValidationRules
+    .ensure('userPhone').required().minLength(10).withMessage('phone number must be at least 10 digits long.')
+    .ensure('userType').required().minLength(5).withMessage('select a user type')
+    .on(this.user);
+  }
+
+  validate() {
+    this.validator.validateObject(this.user);
+  }
+
   async updateUser(){
     let uid = this.auth.getTokenPayload().sub;
     await fetch;
-    this.user.userType = this.selectedValue;
+    //this.user.userType = this.selectedValue;
     this.httpClient.fetch('/user/' + uid, {
       method: 'put',
       body: json(this.user)
