6b52ff7d4ad4511ea132ce5326c2de9b6b21b505
DannyMcwaves <dannymcwaves96@rocketmail.com>
2017-05-31 10:23:30 +0000

    tests for user account updated

--- test/karma-unit/user-account.spec.js
+++ test/karma-unit/user-account.spec.js
@@ -4,7 +4,12 @@ import {App} from '../../src/app';
 describe('the UserAccount Module', () => {
   let ua;
   beforeEach(() => {
-    ua = new UserAccount(AuthStub, App, HttpMock);
+    ua = new UserAccount(new AuthStub(), new App(), new HttpMock());
+  });
+
+  it('should activate user account', (done) => {
+    ua.activate();
+    done();
   });
 
   it('checks deletes the user', (done) => {
