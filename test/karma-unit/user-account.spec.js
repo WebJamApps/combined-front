41c5e04009df6757b3e7dd9821a6890705ab3207
Josh Sherman <joshua.v.sherman@gmail.com>
2017-06-02 10:12:23 -0400

    unit tests

--- test/karma-unit/user-account.spec.js
+++ test/karma-unit/user-account.spec.js
@@ -1,21 +1,20 @@
-// import {UserAccount} from '../../src/dashboard-child-routes/user-account';
-// import {AuthStub, HttpMock} from './commons';
-// import {App} from '../../src/app';
-// describe('the UserAccount Module', () => {
-//   let ua;
-//   let app = new App(AuthStub, HttpMock);
-//   beforeEach(() => {
-//     ua = new UserAccount(app);
-//   });
-//
-//   it('should activate user account', (done) => {
-//     ua.activate();
-//     done();
-//   });
-//
-//   it('checks deletes the user', (done) => {
-//     ua.deleteUser();
-//     //expect(typeof home.widescreen).toBe('boolean');
-//     done();
-//   });
-// });
+import {UserAccount} from '../../src/dashboard-child-routes/user-account';
+import {AuthStub, HttpMock} from './commons';
+import {App} from '../../src/app';
+describe('the UserAccount Module', () => {
+  let ua;
+  let app = new App(new AuthStub(), new HttpMock());
+  beforeEach(() => {
+    ua = new UserAccount(app);
+  });
+
+  it('should activate user account', (done) => {
+    ua.activate();
+    done();
+  });
+
+  it('deletes the user', (done) => {
+    ua.deleteUser();
+    done();
+  });
+});
