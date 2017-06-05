ddbd59d40b400d3001a157debd1a95c47875546e
Josh Sherman <joshua.v.sherman@gmail.com>
2017-06-05 13:28:26 -0400

    data binding to the user for volunteer prefs is now working, yeah!

--- src/dashboard-child-routes/user-account.js
+++ src/dashboard-child-routes/user-account.js
@@ -5,23 +5,52 @@ import {json} from 'aurelia-fetch-client';
 export class UserAccount {
   constructor(app){
     this.app = app;
+    this.selectedCauses = [];
+    this.selectedTalents = [];
+    this.selectedWorks = [];
   }
 
-  causes = ['Christian', 'Environmental', 'Hunger', 'Animal Rights', 'Homeless', 'Veterans', 'Elderly', 'other'];
-  talents = ['music', 'athletics', 'childcare', 'mechanics', 'construction', 'computers', 'communication', 'chess playing', 'listening', 'other'];
-  works = ['hashbrown slinging', 'nail hammering', 'leaf removal', 'floor mopping', 'counseling', 'other'];
-  selectedCause = [];
-  selectedTalent = [];
-  selectedWork = [];
+  causes = ['Christian', 'Environmental', 'Hunger', 'Animal Rights', 'Homeless', 'Veterans', 'Elderly'];
+  talents = ['music', 'athletics', 'childcare', 'mechanics', 'construction', 'computers', 'communication', 'chess playing', 'listening'];
+  works = ['hashbrown slinging', 'nail hammering', 'leaf removal', 'floor mopping', 'counseling', 'visitation'];
+  //selectedCauses = [];
 
   async activate() {
     this.uid = this.app.auth.getTokenPayload().sub;
     this.user = await this.app.appState.getUser(this.uid);
-    //this.app.appState.user;
+    //const tempCause = this.user.volCauses;
+
     this.role = this.user.userType;
     this.causes.sort();
+    this.causes.push('other');
+    for (let i = 0; i < this.causes.length; i++) {
+      if (this.user.volCauses.indexOf(this.causes[i]) > -1){
+        this.selectedCauses.push(this.causes[i]);
+      } else {
+        this.selectedCauses.push('');
+      }
+    }
+    console.log('the selected causes are: ' + this.selectedCauses);
     this.talents.sort();
+    this.talents.push('other');
+    for (let i = 0; i < this.talents.length; i++) {
+      if (this.user.volTalents.indexOf(this.talents[i]) > -1){
+        this.selectedTalents.push(this.talents[i]);
+      } else {
+        this.selectedTalents.push('');
+      }
+    }
+    console.log('the selected talents are: ' + this.selectedTalents);
     this.works.sort();
+    this.works.push('other');
+    for (let i = 0; i < this.works.length; i++) {
+      if (this.user.volWorkPrefs.indexOf(this.works[i]) > -1){
+        this.selectedWorks.push(this.works[i]);
+      } else {
+        this.selectedWorks.push('');
+      }
+    }
+    console.log('the selected talents are: ' + this.selectedTalents);
     if (this.user.userType === 'Charity'){
       this.role = 'Charity Manager';
     }
@@ -52,14 +81,14 @@ export class UserAccount {
   }
 
   causePicked(){
-    if (this.selectedCause.length === 0){
+    if (this.selectedCauses.length === 0){
       this.user.volCauses = [];
       return;
     }
-    this.user.volCauses = this.selectedCause;
-    for (let i = 0; i < this.selectedCause.length; i++) {
-      console.log(this.selectedCause);
-      if (this.selectedCause.includes('other')){
+    this.user.volCauses = this.selectedCauses;
+    for (let i = 0; i < this.selectedCauses.length; i++) {
+      console.log(this.selectedCauses);
+      if (this.selectedCauses.includes('other')){
         console.log('other was selected, we will display an additional form field now');
         this.causeOther = true;
       }
@@ -67,14 +96,14 @@ export class UserAccount {
   }
 
   talentPicked(){
-    if (this.selectedTalent.length === 0){
+    if (this.selectedTalents.length === 0){
       this.user.volTalents = [];
       return;
     }
-    this.user.volTalents = this.selectedTalent;
-    for (let i = 0; i < this.selectedTalent.length; i++) {
-      console.log(this.selectedTalent);
-      if (this.selectedTalent.includes('other')){
+    this.user.volTalents = this.selectedTalents;
+    for (let i = 0; i < this.selectedTalents.length; i++) {
+      console.log(this.selectedTalents);
+      if (this.selectedTalents.includes('other')){
         console.log('other was selected, we will display an additional form field now');
         this.talentOther = true;
       }
@@ -82,14 +111,14 @@ export class UserAccount {
   }
 
   workPicked(){
-    if (this.selectedWork.length === 0){
+    if (this.selectedWorks.length === 0){
       this.user.volWorkPrefs = [];
       return;
     }
-    this.user.volWorkPrefs = this.selectedWork;
-    for (let i = 0; i < this.selectedWork.length; i++) {
-      console.log(this.selectedWork);
-      if (this.selectedWork.includes('other')){
+    this.user.volWorkPrefs = this.selectedWorks;
+    for (let i = 0; i < this.selectedWorks.length; i++) {
+      console.log(this.selectedWorks);
+      if (this.selectedWorks.includes('other')){
         console.log('other was selected, we will display an additional form field now');
         this.workOther = true;
       }
