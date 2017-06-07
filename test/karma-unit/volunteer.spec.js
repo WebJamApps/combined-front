4f2104ce69bc813a0d7f33f8e36e34a0bcf2210d
Josh Sherman <joshua.v.sherman@gmail.com>
2017-06-07 10:37:39 -0400

    volunteering is completed for this release

--- test/karma-unit/volunteer.spec.js
+++ test/karma-unit/volunteer.spec.js
@@ -11,11 +11,26 @@ describe('the Volunteer Module', () => {
     auth.setToken({sub: 'aowifjawifhiawofjo'});
     app = new App(auth, new HttpMock());
     app.appstate = new AppStateStub();
-    app.appstate.user = {_id: '23334', name: 'billy', email: 'billy@billy.com'};
+    //app.appstate.user = {_id: '23334', name: 'billy', email: 'billy@billy.com', volCauses: ['', '']};
     volunteer = new Volunteer(app);
   });
   it('should active so it can display the volunteer settings', (done) => {
     volunteer.activate();
+    done();
+  });
+  it('should run attached to populate the table when no prefs are set', (done) => {
+    volunteer.activate();
+    document.body.innerHTML = '<p id="causes"></p><p id="talents"></p><p id="works"></p>';
+    volunteer.user = {volCauses: [''], volTalents: [''], volWorkPrefs: [''] };
+    volunteer.attached();
+    done();
+  });
+
+  it('should run attached to populate the table when all prefs are set', (done) => {
+    volunteer.activate();
+    document.body.innerHTML = '<p id="causes"></p><p id="talents"></p><p id="works"></p>';
+    volunteer.user = {volCauses: ['Hunger', 'other'], volTalents: ['Cooking', 'other'], volWorkPrefs: ['Chopping', 'other'], volCauseOther: 'Thirst', volTalentOther: 'Singing', volWorkOther: 'Cleaning' };
+    volunteer.attached();
     done();
   });
 });
