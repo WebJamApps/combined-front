6e3ea211a7628f84d62107c2b6d2cc68a7a006c9
M. Adam Kendall <michael.kendall@gsa.gov>
2017-08-12 14:55:53 -0400

    Start the refactor of volunteer opps into own dashboard

--- src/dashboard-router.js
+++ src/dashboard-router.js
@@ -6,6 +6,7 @@ export class DashboardRouter {
     config.map([
       { route: '', name: 'dashboard', moduleId: PLATFORM.moduleName('./dashboard'), nav: false, title: 'Dashboard', auth: true},
       { route: 'charity', name: 'charity', moduleId: PLATFORM.moduleName('./dashboard-child-routes/charity'), nav: true, title: 'Charity', auth: true},
+      { route: 'vol-ops/:id', name: 'vol-ops', moduleId: PLATFORM.moduleName('./dashboard-child-routes/vol-ops'), nav: false, title: 'Volunteer Opportunities', auth: true},
       { route: 'volunteer', name: 'volunteer', moduleId: PLATFORM.moduleName('./dashboard-child-routes/volunteer'), nav: true, title: 'Volunteer', auth: true},
       { route: 'reader', name: 'reader', moduleId: PLATFORM.moduleName('./dashboard-child-routes/reader'), nav: true, title: 'Reader', auth: true},
       { route: 'librarian', name: 'librarian', moduleId: PLATFORM.moduleName('./dashboard-child-routes/librarian'), nav: true, title: 'Librarian', auth: true},
