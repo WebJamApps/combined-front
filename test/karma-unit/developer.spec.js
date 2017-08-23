d0b274d8c6ddc9dca3d4d8a0a5d4a62de9106b32
“Joshua <joshua.v.sherman@gmail.com>
2017-08-23 12:39:19 -0400

    more unit tests

--- test/karma-unit/developer.spec.js
+++ test/karma-unit/developer.spec.js
@@ -1,11 +1,34 @@
 import {Developer} from '../../src/dashboard-child-routes/developer';
 import {App} from '../../src/app';
-import {AuthStub} from './commons';
+import {AuthStub, HttpMock, AppStateStub} from './commons';
+
+class HttpMockDev extends HttpMock {
+  fetch(url, obj) {
+    //console.log(url);
+    this.headers.url = url;
+    this.headers.method = obj ? obj.method : 'GET';
+    if (obj && obj.method === 'put') {
+      this.user = obj.body;
+    }
+    this.status = 200;
+    return Promise.resolve({
+      Headers: this.headers,
+      json: () => Promise.resolve([])
+    });
+  }
+}
 
 describe('the Developer Module', () => {
   let developer;
+  let auth;
+  let app;
   beforeEach(() => {
-    developer = new Developer(AuthStub, App);
+    auth = new AuthStub();
+    auth.setToken({sub: '1'});
+    app = new App(auth, new HttpMockDev());
+    app.activate();
+    developer = new Developer(app);
+    developer.app.appState = new AppStateStub();
   });
 
   it('activates', (done) => {
