82a6d82def3822832b9f2f6d030b458e02a0953f
“Joshua <joshua.v.sherman@gmail.com>
2017-08-28 13:06:56 -0400

    display all events to the volunteer

--- src/dashboard-child-routes/volunteer.js
+++ src/dashboard-child-routes/volunteer.js
@@ -5,15 +5,81 @@ import {App} from '../app';
 export class Volunteer {
   constructor(app){
     this.app = app;
+    this.events = [];
   }
 
   async activate() {
     this.uid = this.app.auth.getTokenPayload().sub;
     this.user = await this.app.appState.getUser(this.uid);
     this.app.dashboardTitle = this.user.userType;
+    const res = await this.app.httpClient.fetch('/volopp/getall');
+    this.events = await res.json();
+    console.log('all events');
+    console.log(this.events);
+    if (this.events.length > 0){
+      this.fixDates();
+      this.buildWorkPrefs();
+      this.buildTalents();
+      //this.charityName = this.events[0].voCharityName;
+    }
   }
 
-  buildCauses(){
+  fixDates(){
+    for (let i = 0; i < this.events.length; i++){
+      let startDate = this.events[i].voStartDate;
+      let endDate = this.events[i].voEndDate;
+      if (startDate !== null){
+        if (startDate.indexOf('T') !== -1){
+          this.events[i].voStartDate = startDate.substr(0, startDate.indexOf('T'));
+        }
+      }
+      if (endDate !== null){
+        if (endDate.indexOf('T') !== -1){
+          this.events[i].voEndDate = endDate.substr(0, endDate.indexOf('T'));
+        }
+      }
+    }
+  }
+
+  buildWorkPrefs(){
+    for (let l = 0; l < this.events.length; l++){
+      let workHtml = '';
+      for (let i = 0; i < this.events[l].voWorkTypes.length; i++) {
+        if (this.events[l].voWorkTypes[i] !== ''){
+          if (this.events[l].voWorkTypes[i] !== 'other'){
+            workHtml = workHtml + '<p style="font-size:10pt; padding-top:4px; margin-bottom:4px">' + this.events[l].voWorkTypes[i] + '</p>';
+          } else {
+            workHtml = workHtml + '<p style="font-size:10pt; padding-top:4px; margin-bottom:4px">' + this.events[l].voWorkTypeOther + '</p>';
+          }
+        }
+      }
+      if (workHtml === ''){
+        workHtml = '<p style="font-size:10pt">not specified</p>';
+      }
+      this.events[l].workHtml = workHtml;
+    }
+  }
+
+  buildTalents(){
+    for (let l = 0; l < this.events.length; l++){
+      let talentHtml = '';
+      for (let i = 0; i < this.events[l].voTalentTypes.length; i++) {
+        if (this.events[l].voTalentTypes[i] !== ''){
+          if (this.events[l].voTalentTypes[i] !== 'other'){
+            talentHtml = talentHtml + '<p style="font-size:10pt; padding-top:4px; margin-bottom:4px">' + this.events[l].voTalentTypes[i] + '</p>';
+          } else {
+            talentHtml = talentHtml + '<p style="font-size:10pt; padding-top:4px; margin-bottom:4px">' + this.events[l].voTalentTypeOther + '</p>';
+          }
+        }
+      }
+      if (talentHtml === ''){
+        talentHtml = '<p style="font-size:10pt">not specified</p>';
+      }
+      this.events[l].talentHtml = talentHtml;
+    }
+  }
+
+  buildUserCauses(){
     let causesHtml = '';
     for (let i = 0; i < this.user.volCauses.length; i++) {
       if (this.user.volCauses[i] !== ''){
@@ -32,7 +98,7 @@ export class Volunteer {
     document.getElementById('causes').innerHTML = causesHtml;
   }
 
-  buildTalents(){
+  buildUserTalents(){
     let talentsHtml = '';
     for (let i = 0; i < this.user.volTalents.length; i++) {
       if (this.user.volTalents[i] !== ''){
@@ -50,7 +116,7 @@ export class Volunteer {
     document.getElementById('talents').innerHTML = talentsHtml;
   }
 
-  buildWorks(){
+  buildUserWorks(){
     let worksHtml = '';
     for (let i = 0; i < this.user.volWorkPrefs.length; i++) {
       if (this.user.volWorkPrefs[i] !== ''){
@@ -68,9 +134,20 @@ export class Volunteer {
     document.getElementById('works').innerHTML = worksHtml;
   }
 
+  showCheckboxes(){
+    const checkboxes = document.getElementById('checkboxes-iron');
+    if (!this.expanded) {
+      checkboxes.opened = true;
+      this.expanded = true;
+    } else {
+      checkboxes.opened = false;
+      this.expanded = false;
+    }
+  }
+
   attached(){
-    this.buildCauses();
-    this.buildTalents();
-    this.buildWorks();
+    this.buildUserCauses();
+    this.buildUserTalents();
+    this.buildUserWorks();
   }
 }
