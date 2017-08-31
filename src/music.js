af15fa781004907ee96ea240093ed91091980132
M. Adam Kendall <michael.kendall@gsa.gov>
2017-08-30 20:04:00 -0400

    Fix up the slideshow with some jquery

--- src/music.js
+++ src/music.js
@@ -3,6 +3,7 @@
 // import {Router} from 'aurelia-router';
 //
 // @inject(App, Router)
+
 export class Music {
   // constructor(app, router){
   //   this.app = app;
@@ -19,27 +20,16 @@ export class Music {
   }
 
   showSlides() {
-    // const slides = document.getElementsByClassName('mySlides');
-    // if (slides !== undefined){
-    //   for (let i = 0; i < slides.length; i++) {
-    //     //console.log(slides[i]);
-    //     if (i === this.slideIndex) {
-    //       slides[i].style.display = 'block';
-    //     } else {
-    //       slides[i].style.display = 'none';
-    //     }
-    //   }
-    //   this.slideIndex++;
-    //   if ((this.slideIndex + 1) > slides.length) {this.slideIndex = 0;}
-    // }
-    // this.counter++;
-    // if (this.counter < 300){
-    //   setTimeout(this.showSlides(), 3000); // Change image every 3 seconds
-    // } else {
-    //   this.activate();
-    // }
+    $('#slideshow > div:first')
+      .hide()
+      .next()
+      .fadeIn(1500)
+      .end()
+      .appendTo('#slideshow');
   }
-  attached(){
-    this.showSlides();
+
+  attached() {
+    $("#slideshow > div:gt(0)").hide();
+    setInterval(this.showSlides, 5000);
   }
 }
