8753b730cd9fe691cd6293c3e8da1cebe9ff754d
“Joshua <joshua.v.sherman@gmail.com>
2017-09-22 11:55:02 -0400

    unit tests

--- test/karma-unit/library.spec.js
+++ test/karma-unit/library.spec.js
@@ -1,26 +1,9 @@
 import {Library} from '../../src/library';
-//import {AppRouterConfig} from '../../src/app.router.config';
-//import {RouterStub} from './commons';
 
 describe('the Library module', () => {
-  //let news1;
-  //let mockedRouter;
-  //let sut;
-  //let config1;
   let lib1;
 
   beforeEach(() => {
-    //mockedRouter = new RouterStub();
-    //sut = new AppRouterConfig(mockedRouter);
-    // config1 = new ConfigStub;
-    // config1.map([
-    //   { route: '', name: 'dashboard', moduleId: './dashboard-routes/dashboard', nav: false, title: 'Dashboard', auth: true},
-    //   { route: 'volunteer', name: 'volunteer', moduleId: './dashboard-routes/volunteer-dashboard', nav: false, title: 'Volunteer', auth: true},
-    //   { route: 'charity', name: 'charity', moduleId: './dashboard-routes/charity-dashboard', nav: false, title: 'Charity', auth: true},
-    //   { route: 'library', name: 'library', moduleId: './dashboard-routes/store-manager-dashboard-router', nav: false, title: 'Store', auth: true}
-    //
-    // ]);
-    //sut.configure(config1, mockedRouter);
     lib1 = new Library();
   });
 
@@ -28,8 +11,9 @@ describe('the Library module', () => {
     expect(typeof lib1.widescreen).toBe('boolean');
     done();
   });
+
   it('runs showslides with widescreen slideshow', (done) => {
-    document.body.innerHTML = '<div id="slideshow1"><div></div><div>';
+    document.body.innerHTML = '<div id="libslideshow1"><div></div><div>';
     lib1.showSlides();
     done();
   });
