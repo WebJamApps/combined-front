2dbcf095cc88d40d48d69801d438d56e6e1274a3
Josh Storo <joshstoro66@gmail.com>
2017-11-05 16:07:15 -0500

    Updated vol-ops tests for more coverage

--- test/karma-unit/vol-ops.spec.js
+++ test/karma-unit/vol-ops.spec.js
@@ -132,8 +132,23 @@ describe('the Volunteer Opps Module', () => {
   });
 
   it('activates and there are events and runs the show time', (done) => {
+
     volops.activate();
     //volops.showTime();
+    done();
+  });
+
+  it("Mark past date", (done)=>{
+    volops.events = [{
+      'voStartDate': '2016-12-12',
+      'voEndDate': '2016-12-12'
+    },
+    {
+      'voStartDate': '2016-12-12',
+      'voEndDate': '2016-12-12'
+    }];
+    volops.markPast();
+    expect(volops.events[0].past).toBe(true);
     done();
   });
   //
@@ -306,7 +321,7 @@ describe('the Volunteer Opps Module', () => {
       'voSignupUserIds': ['1', '2', '3']
     };
     //TODO this same httpmock needs to have a /user/uid that responds with a 404 error (user not found)
-    volops.viewPeople(signupevent);
+    volops4.viewPeople(signupevent);
     done();
   });
 
