e54fd7ea6fe1cb7fce679f9f5f5aa6240bb1578e
Joshua V Sherman <joshua.v.sherman@gmail.com>
2018-01-28 09:35:29 -0500

    new login/reg WIP

--- src/commons/patric.js
+++ src/commons/patric.js
@@ -1,23 +1,3 @@
-// exports.showHideElements = function(appName, pArr, nArr){
-//   let element;
-//   for (let i = 0; i < pArr.length; i++){
-//     element = document.getElementsByClassName(pArr[i])[0];
-//     if (appName === 'PATRIC'){
-//       element.style.display = 'block';
-//     } else {
-//       element.style.display = 'none';
-//     }
-//   }
-//   for (let j = 0; j < nArr.length; j++){
-//     element = document.getElementsByClassName(nArr[j])[0];
-//     if (appName === 'PATRIC'){
-//       element.style.display = 'none';
-//     } else {
-//       element.style.display = 'block';
-//     }
-//   }
-// };
-
 exports.showHideElements2 = function(appName, objofElements) {
   let objKeys = Object.keys(objofElements);
   let element;
@@ -28,7 +8,6 @@ exports.showHideElements2 = function(appName, objofElements) {
       if ((appName === objKeys[i]) || (objKeys[i] !== 'PATRIC' && appName !== 'PATRIC')) {
         document.getElementsByClassName(element)[0].style.display = 'block';
       }
-      //   if (){document.getElementsByClassName(element)[0].style.display = 'block';}
     }
   }
 };
