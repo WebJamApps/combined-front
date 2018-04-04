8d71b85b8c913e595407ef0d95219dc40c110b30
dannymcwaves <dannymcwaves96@gmail.com>
2018-04-04 10:26:30 +0000

    new refactored component

---
+++ src/classes/react-comp.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactElement from './react-element';
+import {noView, inject, customElement, bindable} from 'aurelia-framework';
+
+@noView()
+@inject(Element)
+@customElement('react-component')
+export class ReactComponent {
+  constructor(element) {
+    this.element = element;
+  }
+
+  render() {
+    ReactDOM.render(<ReactElement/>, this.element);
+  }
+
+  bind() {
+    this.render()
+  }
+}
