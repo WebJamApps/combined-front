02bc2c67a2810c668d0012a4c95b4cf50dfa38ea
dannymcwaves <dannymcwaves96@gmail.com>
2018-04-04 10:28:10 +0000

    commented out this section. will fix this.

--- src/react-test.js
+++ src/react-test.js
@@ -1,36 +1,36 @@
-//import React from 'react';
-import ReactDOM from 'react-dom';
-//// { noView, inject } from 'aurelia-framework';
-import {ReactComp} from './classes/ReactComp';
-//@noView()
-//@inject(Element)
-export class ReactTest {
-  constructor(){
-    this.helloWorld;
-    this.reactComp = new ReactComp();
-    console.log(this.reactComp);
-    //this.reactComp = new ReactComp();
-
-    //this.helloworld = new ReactComp();
-  }
-  // render() {
-  //   ReactDOM.render(
-  //    <ReactComp
-  //    />,
-  //    document.getElementsByClassName('page-content')[0]
-  //  );
-  // }
-  attached(){
-//     ReactDOM.render(React.createElement(
-//   'h1',
-//   null,
-//   'Hello, world!'
-// ), document.getElementsByClassName('here')[0]);
-    ReactDOM.render(this.reactComp.render(), document.getElementsByClassName('here')[0]);
-  }
-  //   ReactDOM.render(
-  // <h1>Hello, world!</h1>,
-  // document.getElementById('root')
-  //   );
-  }
-// }
+// //import React from 'react';
+// import ReactDOM from 'react-dom';
+// //// { noView, inject } from 'aurelia-framework';
+// import {ReactComponent} from './classes/react-comp';
+// //@noView()
+// //@inject(Element)
+// export class ReactTest {
+//   constructor(){
+//     this.helloWorld;
+//     this.reactComp = new ReactComp();
+//     console.log(this.reactComp);
+//     //this.reactComp = new ReactComp();
+//
+//     //this.helloworld = new ReactComp();
+//   }
+//   // render() {
+//   //   ReactDOM.render(
+//   //    <ReactComp
+//   //    />,
+//   //    document.getElementsByClassName('page-content')[0]
+//   //  );
+//   // }
+//   attached(){
+// //     ReactDOM.render(React.createElement(
+// //   'h1',
+// //   null,
+// //   'Hello, world!'
+// // ), document.getElementsByClassName('here')[0]);
+//     ReactDOM.render(this.reactComp.render(), document.getElementsByClassName('here')[0]);
+//   }
+//   //   ReactDOM.render(
+//   // <h1>Hello, world!</h1>,
+//   // document.getElementById('root')
+//   //   );
+//   }
+// // }
