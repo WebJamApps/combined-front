c902180be1a24e9546343467a1ba07732119a078
dannymcwaves <dannymcwaves96@gmail.com>
2018-04-09 13:14:20 +0000

    changed the name of the react element file to react-comp and vice versa

--- src/components/react-element.js
+++ src/components/react-element.js
@@ -1,8 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactElement from './react-comp';
+import {noView, inject, customElement} from 'aurelia-framework';
 
-export default class HelloWorld extends Component {
+@noView()
+@inject(Element)
+@customElement('react-component')
+export class ReactComponent {
+  constructor(element) {
+    this.element = element;
+  }
 
   render() {
-    return <h1>Hello World</h1>;
+    ReactDOM.render(<ReactElement/>, this.element);
+  }
+
+  bind() {
+    this.render();
   }
 }
