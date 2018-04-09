433c2013a0646be445729670cf1ea528a1aec50c
dannymcwaves <dannymcwaves96@gmail.com>
2018-04-09 15:33:09 +0000

    time picker component updated to update the voOp end and start dates of the voOp object. cannot validate the object.

--- src/components/time-picker.js
+++ src/components/time-picker.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TimeInput from 'material-ui-time-picker';
-import {noView, customElement, inject} from 'aurelia-framework';
+import {noView, customElement, bindable, inject} from 'aurelia-framework';
 
 @noView()
 @inject(Element)
+@bindable('data')
+@bindable('type')
+@bindable('validator')
 @customElement('time-picker')
 export class TimePicker {
   constructor(element) {
@@ -12,7 +15,15 @@ export class TimePicker {
   }
 
   render() {
-    ReactDOM.render(<TimeInput mode='12h' onChange={(time) => {console.log(time);}} />, this.element);
+    ReactDOM.render(<TimeInput data={this.data} mode='12h' onChange={(time) => {this.updateTime(time)}}/>, this.element);
+  }
+
+  updateTime(time) {
+    if (this.type === 'start') {
+      this.data.voStartTime = time;
+    } else {
+      this.data.voEndTime = time;
+    }
   }
 
   bind() {
