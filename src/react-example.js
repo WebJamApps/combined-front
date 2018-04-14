e92b96aafccd080b7ac8d6657b827ab5803407a9
Joshua V Sherman <joshua.v.sherman@gmail.com>
2018-04-13 21:55:01 -0400

    unit tests

--- src/react-example.js
+++ src/react-example.js
@@ -1,4 +1,13 @@
 
 export class ReactExample {
+  constructor(){
+    this.reactElementInner = null;
+  }
+
+  attached(){
+    //console.log(document.getElementsByClassName('here')[0].children[0].innerHTML);
+    this.reactElementInner = document.getElementsByClassName('here')[0].children[0].innerHTML;
+    console.log(this.reactElementInner);
+  }
 
 }
