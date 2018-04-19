2d5e4a3a67b6820e19f39455ef187c320d7a1871
dannymcwaves <dannymcwaves96@gmail.com>
2018-04-19 15:20:01 +0000

    time picker time offset fixed

--- src/components/time-picker.js
+++ src/components/time-picker.js
@@ -13,13 +13,14 @@ export class TimePicker {
     this.element = element;
     this.updateTime = this.updateTime.bind(this);
     this.el = null;
+    this.picker = null;
     this.showTimer = this.showTimer.bind(this);
   }
 
   get component() {
     return (
       <div>
-        <div style={{display: 'none'}}><TimeInput mode='12h' onChange={this.updateTime}/></div>
+        <div style={{display: 'none'}}><TimeInput ref={(el) => {this.picker = el;}} mode='12h' onChange={this.updateTime}/></div>
         <section ref={(el) => {this.el = el;}} style={{border: '1px solid #ccc', color: '#fff', padding: '1px 5px', width: '83%', margin: 0, outline: 0, textAlign: 'left', cursor: 'text'}} onClick={this.showTimer}>{this.type === 'start' ? '8:00 am' : '5:00 pm'}</section>
       </div>
     );
@@ -35,14 +36,11 @@ export class TimePicker {
   }
 
   updateTime(time) {
-    let tz = time.getTimezoneOffset();
-    console.log(tz);
-    time = time.toISOString().split('T')[1].split(':');
-    let a = parseInt(time[0], 0);
-    let b = time[1].split(':')[0];
+    let a = time.getHours();
+    let b = time.getMinutes();
     let zone = a > 11 && a !== 0 ? 'pm' : 'am';
-    let offset = a > 12 && a !== 0 ? (a % 12) - tz : a === 0 ? 12 - tz : a - tz;
-    this.data = `${offset}:${b} ${zone}`;
+    let offset = a > 12 && a !== 0 ? a % 12 : a === 0 ? 12 : a;
+    this.data = `${offset}:${b < 10 ? '0' + b : b} ${zone}`;
     this.el.style.color = '#000';
     this.el.innerText = this.data;
   }
