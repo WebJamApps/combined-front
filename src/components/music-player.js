b044067a9953c42bc5022ccef678e11f1a520e46
dannymcwaves <dannymcwaves96@gmail.com>
2018-06-24 21:04:35 +0000

    music player component features updated

--- src/components/music-player.js
+++ src/components/music-player.js
@@ -8,37 +8,70 @@ import ReactPlayer from 'react-player';
 @inject(Element)
 @customElement('music-player')
 export class MusicPlayer {
+
   constructor(element) {
     this.element = element;
-    this.url = [
-      {src: 'https://www.youtube.com/embed/ach2ubW21h4', type: 'video/webm'},
-      {src: 'https://www.youtube.com/embed/mCvUBjuzfo8', type: 'video/webm'},
-      {src: 'DG.mp3', type: 'audio/mpeg'},
-      {src: 'MRM.mp3', type: 'audio/mpeg'},
-      {src: 'AT.mp3', type: 'audio/mpeg'},
-      {src: 'TTGA.mp3', type: 'audio/mpeg'}
-    ];
-    this.playing = false;
+    this.urls = ['DG.mp3', 'MRM.mp3', 'AT.mp3', 'TTGA.mp3', 'https://soundcloud.com/joshandmariamusic/good-enough', 'https://www.youtube.com/embed/ach2ubW21h4', 'https://www.youtube.com/embed/mCvUBjuzfo8'];
     this.play = this.play.bind(this);
+    this.index = 0;
+    this.url = this.urls[this.index];
+    this.playing = false;
+    this.playEnd = this.playEnd.bind(this);
+    this.stop = this.stop.bind(this);
+    this.shuffle = this.shuffle.bind(this);
   }
 
   get html() {
     return (
       <div id="player" align="center" className="mb-2">
         <section id="playSection" style={{display: 'inline'}}>
-          <ReactPlayer style={{backgroundColor: '#eee'}} url={this.url} playing={true} />
+          <ReactPlayer style={{backgroundColor: '#eee'}} url={this.url} playing={this.playing} controls={true} onEnded={this.playEnd}/>
         </section>
         <section className="mt-0">
           <button onClick={this.play}>Play</button>
-          <button>Stop</button>
-          <button>Shuffle</button>
+          <button onClick={this.stop}>Stop</button>
+          <button onClick={this.shuffle}>Shuffle</button>
         </section>
       </div>
     )
   }
 
+  /**
+   * Shuffles array in place. ES6 version
+   */
+  shuffle() {
+    for (let i = this.urls.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.urls[i], this.urls[j]] = [this.urls[j], this.urls[i]];
+    }
+  }
+
+  playEnd() {
+    console.log("call this when the play has ended");
+    this.next();
+  }
+
   play() {
     this.playing = true;
+    this.bind();
+  }
+
+  stop () {
+    this.playing = false;
+    this.bind();
+  }
+
+  next() {
+    this.index += 1;
+    if (this.index >= this.urls.length) {
+      this.playing = false;
+      this.index = 0;
+      this.url = this.urls[this.index];
+      this.bind();
+    } else {
+      this.url = this.urls[this.index];
+      this.bind()
+    }
   }
 
   render() {
