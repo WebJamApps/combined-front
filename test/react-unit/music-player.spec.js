8581e0b1b84ec214b41839e7306efb77d24fb49d
dannymcwaves <dannymcwaves96@gmail.com>
2018-06-26 14:12:14 +0000

    tests for the music player

--- test/react-unit/music-player.spec.js
+++ test/react-unit/music-player.spec.js
@@ -1,4 +1,4 @@
-import {MusicPlayer} from "../../src/components/music-player";
+import { MusicPlayer } from '../../src/components/music-player';
 
 
 describe('++MusicPlayer tests', () => {
@@ -6,10 +6,43 @@ describe('++MusicPlayer tests', () => {
 
   beforeEach(() => {
     mp = new MusicPlayer();
+    document.body.innerHTML = '<div id="renderer"><section id="copier"></section><section id="copyMessage"></section></div>';
+    mp.element = document.getElementById('renderer');
   });
 
-  it('should just log mp', () => {
-    console.log(mp);
+  it('shuffle the url and start play', () => {
+    mp.shown = true;
+    mp.shuffle();
+    mp.share();
   });
 
+  it('should simulate play end', () => {
+    mp.playEnd();
+  });
+
+  it('should copy share', () => {
+    mp.share();
+  });
+
+  it('should simulate the stop player', () => {
+    mp.index = 7;
+    mp.next();
+    mp.stop();
+  });
+
+  it('should pause the player', () => {
+    mp.pause();
+  });
+
+  it('should copy the share url', (done) => {
+    mp.navigator = {
+      clipboard: {
+        writeText(data) { return Promise.resolve(data); }
+      }
+    };
+    mp.copyShare();
+    setTimeout(() => {
+      done();
+    }, 1510);
+  });
 });
