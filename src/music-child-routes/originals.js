8a4f510be15d2fbecd03e0c8050350291d0903d2
dannymcwaves <dannymcwaves96@gmail.com>
2018-07-01 18:36:41 +0000

    complete player specs in one play mode

--- src/music-child-routes/originals.js
+++ src/music-child-routes/originals.js
@@ -2,11 +2,18 @@ export class Originals {
   attached() {
     if (document.location.search === '?oneplayer=true') {
       document.getElementById('wholeMusicSection').style.display = 'none';
-      document.getElementById('wjfooter').style.display = 'none';
-      document.getElementsByClassName('home-header')[0].style.display = 'none';
+      document.getElementsByClassName('content-block')[0].style.overflow = 'hidden';
+      document.getElementsByClassName('page-content')[0].style.borderRight = '0';
       document.getElementsByTagName('h4')[0].style.display = 'none';
+
+      const header = document.getElementsByClassName('home-header')[0];
+      const footer = document.getElementById('wjfooter');
+      const i = document.getElementById('mobilemenutoggle');
       const child = document.getElementsByClassName('home-sidebar')[0];
       child.parentNode.removeChild(child);
+      i.parentNode.removeChild(i);
+      footer.parentNode.removeChild(footer);
+      header.parentNode.removeChild(header);
     }
   }
 }
