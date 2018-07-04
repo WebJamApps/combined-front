777cfc88372deeb85644fb506d01e11cf247629e
dannymcwaves <dannymcwaves96@gmail.com>
2018-07-04 21:04:43 +0000

    be able to click in cell phone mode. rm swipe area

--- src/music-child-routes/originals.js
+++ src/music-child-routes/originals.js
@@ -5,14 +5,17 @@ export class Originals {
       document.getElementsByClassName('content-block')[0].style.overflow = 'hidden';
       document.getElementsByClassName('content-block')[0].style.marginTop = '0';
       document.getElementsByClassName('page-content')[0].style.borderRight = '0';
-      document.getElementsByTagName('h4')[0].style.display = 'none';
 
+      const h4 = document.getElementsByTagName('h4')[0];
       const header = document.getElementsByClassName('home-header')[0];
+      const swipe = document.getElementsByClassName('swipe-area')[0];
       const footer = document.getElementById('wjfooter');
       const i = document.getElementById('mobilemenutoggle');
       const child = document.getElementsByClassName('home-sidebar')[0];
       child.parentNode.removeChild(child);
       i.parentNode.removeChild(i);
+      h4.parentNode.removeChild(h4);
+      swipe.parentNode.removeChild(swipe);
       footer.parentNode.removeChild(footer);
       header.parentNode.removeChild(header);
     }
