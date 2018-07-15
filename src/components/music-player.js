543b0295eeb25c0eeb7ff513c91e979ccc5856ab
dannymcwaves <dannymcwaves96@gmail.com>
2018-07-15 19:29:14 +0000

    possible fix for code-climate issue

--- src/components/music-player.js
+++ src/components/music-player.js
@@ -12,12 +12,12 @@ export class MusicPlayer {
     this.urls = [['DG.mp3', 'Don\'t Go / Web Jam Band'], ['MRM.mp3', 'Misty Rainy Morning / Web Jam Band'], ['AT.mp3', 'Alone Time / Web Jam Band'],
       ['TTGA.mp3', 'Try to Get Along / Web Jam Band'], [`https://www.youtube.com/embed/ach2ubW21h4?origin=http://${document.location.host}`,
         'Boogie Board Rash / Josh Sherman'], [`https://www.youtube.com/embed/mCvUBjuzfo8?origin=http://${document.location.host}`,
-        'Hey Red / Josh Sherman'], ['https://soundcloud.com/joshandmariamusic/good-enough', 'Good Enough / Josh & Maria Sherman']];
+          'Hey Red / Josh Sherman'], ['https://soundcloud.com/joshandmariamusic/good-enough', 'Good Enough / Josh & Maria Sherman']];
 
     this._urls = [['DG.mp3', 'Don\'t Go / Web Jam Band'], ['MRM.mp3', 'Misty Rainy Morning / Web Jam Band'], ['AT.mp3', 'Alone Time / Web Jam Band'],
       ['TTGA.mp3', 'Try to Get Along / Web Jam Band'], [`https://www.youtube.com/embed/ach2ubW21h4?origin=http://${document.location.host}`,
         'Boogie Board Rash / Josh Sherman'], [`https://www.youtube.com/embed/mCvUBjuzfo8?origin=http://${document.location.host}`,
-        'Hey Red / Josh Sherman'], ['https://soundcloud.com/joshandmariamusic/good-enough', 'Good Enough / Josh & Maria Sherman']];
+          'Hey Red / Josh Sherman'], ['https://soundcloud.com/joshandmariamusic/good-enough', 'Good Enough / Josh & Maria Sherman']];
 
     this.play = this.play.bind(this);
     this.index = 0;
