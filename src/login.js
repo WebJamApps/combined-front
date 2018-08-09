8cb01b872aa4e722dd3a8a006fd5cd56c11f4a2b
“Joshua <joshua.v.sherman@gmail.com>
2018-08-09 19:34:11 -0400

    Fixes logout from charity

--- src/login.js
+++ src/login.js
@@ -8,10 +8,7 @@ export class Login {
     this.app = app;
     this.login_Class = new Login_();
   }
-  // showLogin(appName) {
-  //   this.login_Class.loginUser(appName);
-  // }
-  // attached() {
-  //   this.app.checkIfLoggedIn();
-  // }
+  attached() {
+    this.app.checkIfLoggedIn();
+  }
 }
