f297e1412a1714e95b51e0989871380be831866f
dannymcwaves <dannymcwaves96@gmail.com>
2018-08-21 22:15:32 +0000

    added props to html

--- src/music-child-routes/mission.js
+++ src/music-child-routes/mission.js
@@ -1,3 +1,26 @@
 export class Mission {
-
+  attached() {
+    // const head = document.getElementById('head');
+    // head.innerHTML += '<meta property="og:title" content="Web Jam Band Original Songs"/>'
+    // + '<meta property="og:description" content="Try out our new music player to hear many songs played in a continous loop"/>'
+    // + '<meta property="og:image" content="https://web-jam.com/static/imgs/webjamlogo1.png"/>';
+    if (document.location.search === '?oneplayer=true') {
+      document.getElementById('wholeMusicSection').style.display = 'none';
+      document.getElementsByClassName('content-block')[0].style.overflow = 'hidden';
+      document.getElementsByClassName('content-block')[0].style.marginTop = '0';
+      document.getElementsByClassName('page-content')[0].style.borderRight = '0';
+      const h4 = document.getElementsByTagName('h4')[0];
+      const header = document.getElementsByClassName('home-header')[0];
+      const swipe = document.getElementsByClassName('swipe-area')[0];
+      const footer = document.getElementById('wjfooter');
+      const i = document.getElementById('mobilemenutoggle');
+      const child = document.getElementsByClassName('home-sidebar')[0];
+      child.parentNode.removeChild(child);
+      i.parentNode.removeChild(i);
+      h4.parentNode.removeChild(h4);
+      swipe.parentNode.removeChild(swipe);
+      footer.parentNode.removeChild(footer);
+      header.parentNode.removeChild(header);
+    }
+  }
 }
