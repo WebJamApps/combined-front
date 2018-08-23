6a8fb132d0dafae75ac6fa68b69ff379298236f9
dannymcwaves <dannymcwaves96@gmail.com>
2018-08-23 17:45:46 +0000

    remove duplicate code

--- src/music-child-routes/originals.js
+++ src/music-child-routes/originals.js
@@ -16,25 +16,4 @@ export class Originals {
       [`https://www.youtube.com/embed/mCvUBjuzfo8?origin=http://${document.location.host}`, 'Hey Red - Josh Sherman'],
       ['https://soundcloud.com/joshandmariamusic/good-enough', 'Good Enough - Josh & Maria Sherman']];
   }
-
-  attached() {
-    if (document.location.search === '?oneplayer=true') {
-      document.getElementById('wholeMusicSection').style.display = 'none';
-      document.getElementsByClassName('content-block')[0].style.overflow = 'hidden';
-      document.getElementsByClassName('content-block')[0].style.marginTop = '0';
-      document.getElementsByClassName('page-content')[0].style.borderRight = '0';
-      const h4 = document.getElementsByTagName('h4')[0];
-      const header = document.getElementsByClassName('home-header')[0];
-      const swipe = document.getElementsByClassName('swipe-area')[0];
-      const footer = document.getElementById('wjfooter');
-      const i = document.getElementById('mobilemenutoggle');
-      const child = document.getElementsByClassName('home-sidebar')[0];
-      child.parentNode.removeChild(child);
-      i.parentNode.removeChild(i);
-      h4.parentNode.removeChild(h4);
-      swipe.parentNode.removeChild(swipe);
-      footer.parentNode.removeChild(footer);
-      header.parentNode.removeChild(header);
-    }
-  }
 }
