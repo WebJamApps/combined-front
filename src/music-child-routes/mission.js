a72a79d7504bb820db4f2e0c329a36fb3845d478
dannymcwaves <dannymcwaves96@gmail.com>
2018-08-24 19:49:38 +0000

    change music name

--- src/music-child-routes/mission.js
+++ src/music-child-routes/mission.js
@@ -1,6 +1,6 @@
 export class Mission {
   constructor() {
-    this.urls = [['DITR.mp3', 'Down In The River - Web Jam Band'],
+    this.urls = [['DITR.mp3', 'Down In The Rain - Web Jam Band'],
       ['https://soundcloud.com/joshandmariamusic/alleluia-alleluia-give-thanks', 'Alleluia Alleluia Give Thanks - Web Jam Band'],
       ['https://soundcloud.com/joshandmariamusic/comeallyoupeople', 'Come All You People - Web Jam Band'],
       ['https://soundcloud.com/joshandmariamusic/ithelordofseaandskyhereiamlord', 'I The Lord Of Sea And Sky Here I Am Lord - Web Jam Band'],
