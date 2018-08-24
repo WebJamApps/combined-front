b5a508ab96e87e90e85cf7639643d90ba31ce9af
dannymcwaves <dannymcwaves96@gmail.com>
2018-08-24 20:58:23 +0000

    changed song titles

--- src/music-child-routes/mission.js
+++ src/music-child-routes/mission.js
@@ -1,12 +1,12 @@
 export class Mission {
   constructor() {
-    this.urls = [['DITR.mp3', 'Down In The Rain - Web Jam Band'],
+    this.urls = [['DITR.mp3', 'Down In The River - Web Jam Band'],
       ['https://soundcloud.com/joshandmariamusic/alleluia-alleluia-give-thanks', 'Alleluia Alleluia Give Thanks - Web Jam Band'],
       ['https://soundcloud.com/joshandmariamusic/comeallyoupeople', 'Come All You People - Web Jam Band'],
       ['https://soundcloud.com/joshandmariamusic/ithelordofseaandskyhereiamlord', 'I The Lord Of Sea And Sky Here I Am Lord - Web Jam Band'],
       ['https://soundcloud.com/joshandmariamusic/one-bread-one-body', 'One Bread One Body - Josh Sherman'],
       ['https://soundcloud.com/joshandmariamusic/canticle-for-departure', 'Canticle For Departure - Josh Sherman']];
-    this._urls = [['DITR.mp3', 'Down In The Rain - Web Jam Band'],
+    this._urls = [['DITR.mp3', 'Down In The River - Web Jam Band'],
       ['https://soundcloud.com/joshandmariamusic/alleluia-alleluia-give-thanks', 'Alleluia Alleluia Give Thanks - Web Jam Band'],
       ['https://soundcloud.com/joshandmariamusic/comeallyoupeople', 'Come All You People - Web Jam Band'],
       ['https://soundcloud.com/joshandmariamusic/ithelordofseaandskyhereiamlord', 'I The Lord Of Sea And Sky Here I Am Lord - Web Jam Band'],
