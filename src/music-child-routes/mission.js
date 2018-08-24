d3319bef223278b5a8da7070725461038b78eff9
dannymcwaves <dannymcwaves96@gmail.com>
2018-08-24 21:59:07 +0000

    change author for mission music

--- src/music-child-routes/mission.js
+++ src/music-child-routes/mission.js
@@ -1,16 +1,16 @@
 export class Mission {
   constructor() {
-    this.urls = [['DITR.mp3', 'Down In The River - Web Jam Band'],
-      ['https://soundcloud.com/joshandmariamusic/alleluia-alleluia-give-thanks', 'Alleluia Alleluia Give Thanks - Web Jam Band'],
-      ['https://soundcloud.com/joshandmariamusic/comeallyoupeople', 'Come All You People - Web Jam Band'],
-      ['https://soundcloud.com/joshandmariamusic/ithelordofseaandskyhereiamlord', 'I The Lord Of Sea And Sky Here I Am Lord - Web Jam Band'],
-      ['https://soundcloud.com/joshandmariamusic/one-bread-one-body', 'One Bread One Body - Josh Sherman'],
-      ['https://soundcloud.com/joshandmariamusic/canticle-for-departure', 'Canticle For Departure - Josh Sherman']];
-    this._urls = [['DITR.mp3', 'Down In The River - Web Jam Band'],
-      ['https://soundcloud.com/joshandmariamusic/alleluia-alleluia-give-thanks', 'Alleluia Alleluia Give Thanks - Web Jam Band'],
-      ['https://soundcloud.com/joshandmariamusic/comeallyoupeople', 'Come All You People - Web Jam Band'],
-      ['https://soundcloud.com/joshandmariamusic/ithelordofseaandskyhereiamlord', 'I The Lord Of Sea And Sky Here I Am Lord - Web Jam Band'],
-      ['https://soundcloud.com/joshandmariamusic/one-bread-one-body', 'One Bread One Body - Josh Sherman'],
-      ['https://soundcloud.com/joshandmariamusic/canticle-for-departure', 'Canticle For Departure - Josh Sherman']];
+    this.urls = [['DITR.mp3', 'Down In The River - Josh & Maria Sherman'],
+      ['https://soundcloud.com/joshandmariamusic/alleluia-alleluia-give-thanks', 'Alleluia Alleluia Give Thanks - Josh & Maria Sherman'],
+      ['https://soundcloud.com/joshandmariamusic/comeallyoupeople', 'Come All You People - Josh & Maria Sherman'],
+      ['https://soundcloud.com/joshandmariamusic/ithelordofseaandskyhereiamlord', 'I The Lord Of Sea And Sky Here I Am Lord - Josh & Maria Sherman'],
+      ['https://soundcloud.com/joshandmariamusic/one-bread-one-body', 'One Bread One Body - Josh & Maria Sherman'],
+      ['https://soundcloud.com/joshandmariamusic/canticle-for-departure', 'Canticle For Departure - Josh & Maria Sherman']];
+    this._urls = [['DITR.mp3', 'Down In The River - Josh & Maria Sherman'],
+      ['https://soundcloud.com/joshandmariamusic/alleluia-alleluia-give-thanks', 'Alleluia Alleluia Give Thanks - Josh & Maria Sherman'],
+      ['https://soundcloud.com/joshandmariamusic/comeallyoupeople', 'Come All You People - Josh & Maria Sherman'],
+      ['https://soundcloud.com/joshandmariamusic/ithelordofseaandskyhereiamlord', 'I The Lord Of Sea And Sky Here I Am Lord - Josh & Maria Sherman'],
+      ['https://soundcloud.com/joshandmariamusic/one-bread-one-body', 'One Bread One Body - Josh & Maria Sherman'],
+      ['https://soundcloud.com/joshandmariamusic/canticle-for-departure', 'Canticle For Departure - Josh & Maria Sherman']];
   }
 }
