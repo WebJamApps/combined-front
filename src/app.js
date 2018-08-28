fbb9b29da6750d5889c46f21858c02ac7f7ddd60
dannymcwaves <dannymcwaves96@gmail.com>
2018-08-28 19:31:30 +0000

    avoid error in console in one player mode

--- src/app.js
+++ src/app.js
@@ -402,10 +402,10 @@ export class App {
     });
     this.manager.on('swipe', this.close.bind(this));
     if (document.location.search === '?oneplayer=true') {
-      document.getElementById('wholeMusicSection').style.display = 'none';
       document.getElementsByClassName('content-block')[0].style.overflow = 'hidden';
       document.getElementsByClassName('content-block')[0].style.marginTop = '0';
       document.getElementsByClassName('page-content')[0].style.borderRight = '0';
+      const wms = document.getElementById('wholeMusicSection'); // .style.display = 'none';
       const h4 = document.getElementsByTagName('h4')[0];
       const header = document.getElementsByClassName('home-header')[0];
       const swipe = document.getElementsByClassName('swipe-area')[0];
@@ -414,6 +414,7 @@ export class App {
       const child = document.getElementsByClassName('home-sidebar')[0];
       child.parentNode.removeChild(child);
       i.parentNode.removeChild(i);
+      wms.parentNode.removeChild(wms);
       h4.parentNode.removeChild(h4);
       swipe.parentNode.removeChild(swipe);
       footer.parentNode.removeChild(footer);
