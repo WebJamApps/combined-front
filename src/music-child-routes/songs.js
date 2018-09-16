7cb2821cd43dbad7625bfbe74198e85abd4e1836
dannymcwaves <dannymcwaves96@gmail.com>
2018-09-16 13:51:27 +0400

    all song categories to populate music players

--- src/music-child-routes/songs.js
+++ src/music-child-routes/songs.js
@@ -1,3 +1,13 @@
-export class Pub {
+import data from '../../config.json';
 
+
+export class Songs {
+  oUrls = data.original.urls;
+  _oUrls = data.original._urls;
+
+  pUrls = data.pub.urls;
+  _pUrls = data.pub._urls;
+
+  mUrls = data.mission.urls;
+  _mUrls = data.mission._urls;
 }
