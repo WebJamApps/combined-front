3e4634e38a9a9ef557507277130075cb49a3f7af
dannymcwaves <dannymcwaves96@gmail.com>
2018-09-17 20:27:54 +0400

    add controlllers/handlers for click.

--- src/music-child-routes/songs.js
+++ src/music-child-routes/songs.js
@@ -10,4 +10,34 @@ export class Songs {
 
   mUrls = data.mission.urls;
   _mUrls = data.mission._urls;
+
+  constructor() {
+    this.originalElement = null;
+    this.missionElement = null;
+    this.pubElement = null;
+  }
+
+  originalClick() {
+    this.originalElement.classList.remove('d-none');
+    this.missionElement.classList.add('d-none');
+    this.pubElement.classList.add('d-none');
+  }
+
+  missionClick() {
+    this.originalElement.classList.add('d-none');
+    this.missionElement.classList.remove('d-none');
+    this.pubElement.classList.add('d-none');
+  }
+
+  pubClick() {
+    this.originalElement.classList.add('d-none');
+    this.missionElement.classList.add('d-none');
+    this.pubElement.classList.remove('d-none');
+  }
+
+  attached() {
+    this.originalElement = document.getElementById('original');
+    this.missionElement = document.getElementById('mission');
+    this.pubElement = document.getElementById('pub');
+  }
 }
