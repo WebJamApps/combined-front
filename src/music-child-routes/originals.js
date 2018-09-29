d20781664e8b10ce89d3c9459748f40ca1f53bab
dannymcwaves <dannymcwaves96@gmail.com>
2018-09-29 18:48:28 +0400

    filter songs by originals and not original

--- src/music-child-routes/originals.js
+++ src/music-child-routes/originals.js
@@ -2,6 +2,6 @@ import data from '../../config.json';
 
 
 export class Originals {
-  urls = data.songs.filter(song => song.category === 'original');
-  _urls = Array.from(data.songs.filter(song => song.category === 'original'));
+  urls = data.songs.filter(song => song.category === 'originals');
+  _urls = Array.from(data.songs.filter(song => song.category === 'originals'));
 }
