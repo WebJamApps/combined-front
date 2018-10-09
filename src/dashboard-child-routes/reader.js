f810b488e4ec082c3094f4feb109bf6b278f3d13
“Joshua <joshua.v.sherman@gmail.com>
2018-10-09 19:24:51 -0400

    unit tests

--- src/dashboard-child-routes/reader.js
+++ src/dashboard-child-routes/reader.js
@@ -8,7 +8,6 @@ export class Reader {
   }
 
   async activate() {
-    // this.app.configHttpClient();
     await fetch;
     const res = await this.app.httpClient.fetch('/book/getall');
     this.books = await res.json();
@@ -22,24 +21,11 @@ export class Reader {
     // double check that someone else didn't already check out this book
     const res = await this.app.httpClient.fetch(`/book/${tempBook._id}`);
     this.book = await res.json();
-    // console.log('I only want to check it out if it is available?');
-    // console.log(this.book);
     if (this.book.checkedOutBy === '' || this.book.checkedOutBy === undefined) {
       this.book.checkedOutBy = this.uid;
-      // console.log(`user id of checkout by: ${this.book.checkedOutBy}`);
       this.book.checkedOutByName = this.user.name;
       this.updateBook(this.book);
-      // this.app.httpClient.fetch('/book/' + this.book._id, {
-      //   method: 'put',
-      //   body: json(this.book)
-      // })
-      // .then((response) => response.json())
-      // .then((data) => {
-      //   //fetch a new list of all books
-      //   this.activate();
-      // });
     } else {
-      // console.log('book is already checked out');
       this.activate();
     }
   }
