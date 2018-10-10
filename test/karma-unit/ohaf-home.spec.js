49b72bc2a223f1370de594f62c540a4e57232128
“Joshua <joshua.v.sherman@gmail.com>
2018-10-10 08:30:32 -0400

    unit tests

--- test/karma-unit/ohaf-home.spec.js
+++ test/karma-unit/ohaf-home.spec.js
@@ -11,25 +11,11 @@ describe('the OhafHome Module', () => {
     expect(typeof truth).toBe('boolean');
     done();
   });
-  // it('show slides when component is attached and there is no slide', (done) => {
-  //   jasmine.clock().install();
-  //   ohaf.attached();
-  //   document.body.innerHTML = '';
-  //   jasmine.clock().tick(5500);
-  //   expect(document.body.innerText).toBe('');
-  //   done();
-  //   jasmine.clock().uninstall();
-  // });
-  // it('shows slides when component is attached', (done) => {
-  //   jasmine.clock().install();
-  //   document.body.innerHTML = '<div id="musicSlide1"><div></div></div><div id="slideshow1"><div class="mySlides">' +
-  //   '<img src="https://static.pexels.com/photos/2422/sky-earth-galaxy-universe.jpg" /></div></div> <div id="musicSlide2"><div>' +
-  //   '</div></div><div id="slideshow"><div class="mySlides"><img src="https://static.pexels.com/photos/2422/sky-earth-galaxy-universe.jpg" />' +
-  //   '</div></div>';
-  //   ohaf.attached();
-  //   jasmine.clock().tick(5500);
-  //   expect(document.getElementById('musicSlide1')).not.toBe(null);
-  //   done();
-  //   jasmine.clock().uninstall();
-  // });
+  it('attaches to the dom', (done) => {
+    let isAttached = false;
+    ohaf.startSlides = function startSlides() { return true; };
+    isAttached = ohaf.attached();
+    expect(isAttached).toBe(true);
+    done();
+  });
 });
