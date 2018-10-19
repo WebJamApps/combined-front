2d23f32e9c6234e3e40c5974b48c33229dd6cedd
“Joshua <joshua.v.sherman@gmail.com>
2018-10-19 18:05:24 -0400

    finishes unit tests for common utils

--- test/jest-unit/commonUtils.spec.js
+++ test/jest-unit/commonUtils.spec.js
@@ -1,4 +1,5 @@
 const sinon = require('sinon');
+const filesaver = require('file-saver');
 const utils = require('../../src/commons/utils');
 const showSlides = require('../../src/commons/showSlides');
 
@@ -14,5 +15,15 @@ describe('the common utils', () => {
     jest.advanceTimersByTime(5400);
     sMock.restore();
     done();
+  });
+  it('makes a tab delimted text file', async () => {
+    let cb;
+    const fMock = sinon.mock(filesaver);
+    fMock.expects('saveAs').resolves(true);
+    try {
+      cb = await utils.makeCSVfile({ fetch() { return Promise.resolve({ json() { return Promise.resolve({}); } }); } }, '', '');
+      expect(cb).toBe(true);
+    } catch (e) { throw e; }
+    fMock.restore();
   });
 });
