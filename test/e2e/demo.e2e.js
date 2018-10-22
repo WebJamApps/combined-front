d5f07868064e571d3c269b7957c2c22165e14953
“Joshua <joshua.v.sherman@gmail.com>
2018-10-22 07:21:03 -0400

    protractro

--- test/e2e/demo.e2e.js
+++ test/e2e/demo.e2e.js
@@ -12,30 +12,12 @@ describe('aurelia skeleton app', () => {
     // poWelcome = new PageObjectWelcome();
 
     await browser.loadAndWaitForAureliaPage(`http://localhost:${config.port}`);
-    // browser.manage().window().setSize(new Dimension(1600,900));
+    // browser.manage().window().setSize(new Dimension(1600, 900));
   });
 
-  // it('should load the page and display the initial page title', async () => {
-  //   await expect(poSkeleton.getCurrentPageTitle()).toBe('Web Jam LLC');
-  // });
-
-  // it('should display greeting', () => {
-  //   expect(poWelcome.getGreeting()).toBe('Welcome to the Aurelia Navigation App!');
-  // });
-
-  // it('should automatically write down the fullname', () => {
-  //   poWelcome.setFirstname('John');
-  //   poWelcome.setLastname('Doe');
-  //
-  //   // For now there is a timing issue with the binding.
-  //   // Until resolved we will use a short sleep to overcome the issue.
-  //   browser.sleep(200);
-  //   expect(poWelcome.getFullname()).toBe('JOHN DOE');
-  // });
-  //
-  // it('should show alert message when clicking submit button', () => {
-  //   expect(poWelcome.openAlertDialog()).toBe(true);
-  // });
+  it('should load the page and display the initial page title', async () => {
+    await expect(poSkeleton.getCurrentPageTitle()).toBe('Web Jam LLC');
+  });
 
   it('should navigate to Music page', async () => {
     await poSkeleton.navigateTo('/music');
@@ -47,18 +29,18 @@ describe('aurelia skeleton app', () => {
     await expect(poSkeleton.getCurrentPageTitle()).toBe('OHAF | Web Jam LLC');
   });
 
-  // it('should navigate to SC2RS page', async () => {
-  //   await poSkeleton.navigateTo('/sc2rs');
-  //   await expect(poSkeleton.getCurrentPageTitle()).toBe('SC2RS | Web Jam LLC');
-  // });
+  it('should navigate to SC2RS page', async () => {
+    await poSkeleton.navigateTo('/sc2rs');
+    await expect(poSkeleton.getCurrentPageTitle()).toBe('SC2RS | Web Jam LLC');
+  });
 
-  // it('should navigate to Library page', async () => {
-  //   await poSkeleton.navigateTo('/library');
-  //   await expect(poSkeleton.getCurrentPageTitle()).toBe('Library | Web Jam LLC');
-  // });
+  it('should navigate to Library page', async () => {
+    await poSkeleton.navigateTo('/library');
+    await expect(poSkeleton.getCurrentPageTitle()).toBe('Library | Web Jam LLC');
+  });
 
-  // it('should navigate to Login page', async () => {
-  //   await poSkeleton.navigateTo('/login');
-  //   await expect(poSkeleton.getCurrentPageTitle()).toBe('Login | Web Jam LLC');
-  // });
+  it('should navigate to Login page', async () => {
+    await poSkeleton.navigateTo('/login');
+    await expect(poSkeleton.getCurrentPageTitle()).toBe('Login | Web Jam LLC');
+  });
 });
