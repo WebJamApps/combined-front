9e5b406293d42f97b9e989859da0a40f8af610a3
“Joshua <joshua.v.sherman@gmail.com>
2018-10-23 08:19:41 -0400

    debug circleci

--- test/e2e/skeleton.po.js
+++ test/e2e/skeleton.po.js
@@ -3,9 +3,18 @@ export class PageObjectSkeleton {
     return browser.getTitle();
   }
 
+  sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+
   async navigateTo(href) {
     const navigatingReady = browser.waitForRouterComplete();
     await element(by.css(`a[href="${href}"]`)).click();
+    console.log('did I get here or fail before?')
+    await this.sleep(1000);
     await navigatingReady;
   }
 }
