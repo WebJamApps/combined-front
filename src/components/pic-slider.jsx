776971b7dc9f2e2df551b78f7e61f180a85e1ec7
dannymcwaves <dannymcwaves96@gmail.com>
2018-11-21 16:47:55 +0000

    remove arrows from the slider

--- src/components/pic-slider.jsx
+++ src/components/pic-slider.jsx
@@ -1,7 +1,7 @@
 import { noView, inject, customElement, bindable } from 'aurelia-framework';
-import Slider from 'react-slick';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
@@ -9,7 +9,7 @@ import 'slick-carousel/slick/slick-theme.css';
 @inject(Element)
 @customElement('picture-slider')
 @bindable('data')
-export class HelloWorld {
+export class PicSlider {
   constructor(element) {
     this.element = element;
     this.settings = {
@@ -18,22 +18,25 @@ export class HelloWorld {
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      arrows: false
     };
   }
 
-  get html() {
+  html() {
     return (
-      <Slider {...this.settings}>
-        {this.data.map((data, key) => {
-          return <div key={key}><img src={data}/></div>
-        })}
-      </Slider>
+      <div>
+        <Slider {...this.settings}>
+          {this.data.map((data, key) => {
+            return <div key={key}><img width='100%' height='100%' src={data}/></div>
+          })}
+        </Slider>
+      </div>
     )
   }
 
   render() {
-    ReactDOM.render(this.html, this.element);
+    ReactDOM.render(this.html(), this.element);
   }
 
   bind() {
