9ac97918531f7b4cc776537801960daa9c847ba3
dannymcwaves <dannymcwaves96@gmail.com>
2018-11-21 16:48:33 +0000

    tests for the slider compoenent

---
+++ test/react-unit/picSlider.spec.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { PicSlider } from '../../src/components/pic-slider';
+
+describe('the App module', () => {
+  let ps;
+  beforeEach(() => {
+    document.body.innerHTML = '<div id="renderer" class="swipe-area"></div>';
+    ps = new PicSlider();
+    ps.data = [
+      '../static/imgs/ohaf/slideshow2.png',
+      '../static/imgs/ohaf/slideshow3.png',
+      '../static/imgs/ohaf/slideshow4.png',
+      '../static/imgs/ohaf/slideshow5.png',
+      '../static/imgs/ohaf/slideshow6.png'
+    ];
+    ps.element = document.getElementById('renderer');
+  });
+  it('should bind empty', (done) => {
+    // ps.bind();
+    done();
+  });
+});
