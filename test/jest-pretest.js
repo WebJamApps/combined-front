17c0b2d35d3102de23b5a2220b62b37b24ad3116
dannymcwaves <dannymcwaves96@gmail.com>
2018-11-21 16:47:25 +0000

    stub matchMedia on the window object

--- test/jest-pretest.js
+++ test/jest-pretest.js
@@ -5,4 +5,12 @@ import * as path from 'path';
 
 Options.relativeToDir = path.join(__dirname, 'react-unit');
 
+window.matchMedia = window.matchMedia || function match() {
+  return {
+    matches: false,
+    addListener: function addlistener() {},
+    removeListener: function rmlistener() {}
+  };
+};
+
 globalize();
