9e5f7e875537ba38a2d57e2b600a0ad3c98a9524
dannymcwaves <dannymcwaves96@gmail.com>
2018-12-23 00:29:56 +0000

    remove redundant js code for slider component

--- src/library.js
+++ src/library.js
@@ -5,26 +5,13 @@ export class Library {
   @bindable
   columnWidth = '450px';
 
-  slideshowImages = ['../static/imgs/library/books1.jpg',
+  slideshowImages = [
+    '../static/imgs/library/books1.jpg',
     '../static/imgs/library/books2.jpg',
-    '../static/imgs/library/books3.jpg'];
-
-  constructor() {
-    this.slideshow_data = {
-      id: 'libslideshow1',
-      slideshow_images: this.slideshowImages
-    };
-    this.slideshow_data2 = {
-      id: 'libslideshow',
-      slideshow_images: this.slideshowImages
-    };
-  }
+    '../static/imgs/library/books3.jpg'
+  ];
 
   get widescreen() {
     return document.documentElement.clientWidth > 1000;
-  }
-
-  attached() {
-    startSlides(['lSlide1', 'lSlide2'], 'you left the library page', ['libslideshow1', 'libslideshow'], document);
   }
 }
