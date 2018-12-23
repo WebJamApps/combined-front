7da82a39a19e7ff6204c7bbf75504afa5cdbca08
dannymcwaves <dannymcwaves96@gmail.com>
2018-12-23 00:31:28 +0000

    remove unused sliding controllers for ohaf-home pic sliders

--- src/ohaf-home.js
+++ src/ohaf-home.js
@@ -19,23 +19,7 @@ export class OhafHome {
 
   slides = data.slides;
 
-  constructor() {
-    this.startSlides = startSlides;
-    this.slideshow_data = {
-      id: 'slideshow1',
-      slideshow_images: this.slideshowImages
-    };
-    this.slideshow_data2 = {
-      id: 'slideshow',
-      slideshow_images: this.slideshowImages
-    };
-  }
-
   get widescreen() {
     return document.documentElement.clientWidth > 1100;
-  }
-
-  attached() {
-    return this.startSlides(['musicSlide1', 'musicSlide2'], 'you left the ohaf page', ['slideshow1', 'slideshow'], document);
   }
 }
